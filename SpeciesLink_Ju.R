729d1d7ad77439688f7fb5b83ba3cdbe861a2b9e
Julia Niemeyer <julia.niemeyer@gmail.com>
2020-04-09 18:29:29 -0300

    Error in geo.clean - Testing coordinate validity Flagged 0 records. Testing equal lat/lon Error in abs(x[[lon]]) : non-numeric argument to mathematical function

--- SpeciesLink_Ju.R
+++ SpeciesLink_Ju.R
@@ -1,7 +1,8 @@
 getwd()
 #mudar para uma das minhas sp.(mamifero) *Blastocerus dichotomus*
 #2. mudar para uma das minhas sp.(planta) *Blanchetia heterotricha*
-spLink <- rspeciesLink(filename = "ex01", scientificName =  c("Blastocerus dichotomus", "Blanchetia heterotricha"), Coordinates = "Yes" )
+spLink <- rspeciesLink (filename = "raw_data", scientificName =  c("Blastocerus dichotomus", "Blanchetia heterotricha"), Coordinates = "Yes" )
+
 table.spLink <- as.data.frame(spLink)
 #View(table.spLink)
 
@@ -15,14 +16,16 @@ splink.coord <- table.spLink[!is.na(table.spLink$data.decimalLatitude) & !is.na(
 #nrow(splink.coord)
 #View(splink.coord)
 
-#Clean lat and long 0s
-splink.coord2 <- splink.coord[splink.coord$data.decimalLatitude != 0 & splink.coord$data.decimalLongitude != 0,]
-#nrow(splink.coord2)
-#View(splink.coord2)
 
+# output w/ only potential correct coordinates
+geo.clean <- clean_coordinates(x = splink.coord,
+                               lon = "data.decimalLongitude",
+                               lat = "data.decimalLatitude",
+                               species = "data.scientificName",
+                               value = "clean")
 
-occs.new.geo <- clean_coordinates(x = splink.coord2,
-                                  lon = "data.decimalLongitude",
-                                  lat = "data.decimalLatitude",
-                                  species = "data.scientificName",
-                                  )
+dir.create("./results/clean_data")
+write.csv(occs,
+          "./results/data/raw_data.csv",
+          row.names = FALSE)
+
