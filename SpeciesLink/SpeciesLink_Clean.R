4110dd7230347555e76a2c949e19f1f4a1969aab
Julia Niemeyer <julia.niemeyer@gmail.com>
2020-05-12 11:57:26 -0300

    Scientific Status is added to the result tables.
    Need to add temporal filter

--- SpeciesLink/SpeciesLink_Clean.R
+++ SpeciesLink/SpeciesLink_Clean.R
@@ -26,15 +26,14 @@ library(rocc)
 
 
 # Read the spreadsheet with the names of all your species (should be located in the "data" folder).
-Sptable <- read.csv("./data/Aves.csv")
+Sptable <- read.csv("./data/Species")
 # Create a vector with the names
 scientificName <- as.vector(Sptable$Species)
 #Create a different folder inside results for clean tables (keep it organized)
 dir.create("./results/clean_data")
 
 #start the data gathering & cleaning
-#for (a in 1:length(scientificName)) {
-for (a in 1:4) {
+for (a in 1:length(scientificName)) {
 ## get records from SpeciesLink and save ir to the "results" directory (the creation of the results directory is part of the rspeciesLink function)
 r <- rspeciesLink(filename = paste0("raw_data_SpeciesLink_",scientificName[a]), scientificName = scientificName[a], Coordinates = "Yes")
 
@@ -64,30 +63,35 @@ if (is.null(dim(r$data))) {
   ##Check status - function by Sara Mortara
   check_status_gbif <- check_string(scientificName = gbif$scientificName)
   head(check_status_gbif)
+  gbif$scientificStatus <- check_status_gbif$scientificName_status
   gbif$scientificName <- check_status_gbif$scientificName_new
 
+  ##WORK ON THIS
+  ##Here the script needs to replace name only IF NAME IS VALID (e.g. if "indet" > discart ) ## TO DO // need to see if other type of status is not valid!
+  gbif <- subset(gbif, scientificStatus != "indet")
+
   #Get onlye species names, lat, long and ID. Rename columns of the two datasets to be the same. Add a column that provides info about data source.
-gbif_table <- data.frame(gbif$key, gbif$scientificName, gbif$decimalLatitude, gbif$decimalLongitude)
+  gbif_table <- data.frame(gbif$key, gbif$scientificName, gbif$decimalLatitude, gbif$decimalLongitude, gbif$scientificStatus)
 
   gbif_table$DataSource <- "GBIF"
   names(gbif_table)[names(gbif_table) == "gbif.key"] <- "ID"
   names(gbif_table)[names(gbif_table) == "gbif.scientificName"] <- "species"
   names(gbif_table)[names(gbif_table) == "gbif.decimalLatitude"] <- "decimalLatitude"
   names(gbif_table)[names(gbif_table) == "gbif.decimalLongitude"] <- "decimalLongitude"
-  message("New taxonomy")
-  head(gbif_table)
+  names(gbif_table)[names(gbif_table) == "gbif.scientificStatus"] <- "scientificStatus"
 
   # output w/ only potential correct coordinates
 
-  # Cleaning data -------------------------------------
-  message("Starting to clean GBIF only.")
-
+  # Cleaning data ------------------------------------
   #Clean NAs
 gbif_table_coord <- gbif_table[!is.na(gbif_table$decimalLatitude) & !is.na(gbif_table$decimalLongitude),]
   #Set rownames to NULL so it does not get errors in geo_clean function
 rownames(gbif_table_coord) <- NULL
   #nrow(gbif_table_coord)
   #View(gbif_table_coord)
+
+#In case geo_clean finds specific rows with errors, use this to clean it. Then continue manually.
+#gbif_table_coord <- gbif_table_coord[-c(row1, row2, row1...), ]
 
   #Now we will use the the function `clean_coordinates()` from the `CoordinateCleaner` package to clean the species records. This function checks for common errors in coordinates such as institutional coordinates, sea coordinates, outliers, zeros, centroids, etc. This function does not accept not available information (here addressed as "NA") so we will first select only data that have a numerical value for both latitude and longitude.
 
@@ -110,8 +114,8 @@ rownames(gbif_table_coord) <- NULL
   #############################################
   ########### Important: TO DO
 
-
   ##Check if species has more than 20 occurences in total after cleaning. If not, than it can't be used in ENM and should not be save (Stockwell & Peterson, 2002).
+
   #############################################
 
   ### If is has more than 20 occurences, than keep it and sabe it to a csv file.
@@ -131,7 +135,83 @@ gbif_input <- occ_search(scientificName = scientificName[a],
                    return = "data")
 
 if (is.null(dim(gbif_input))) {
-  message("Output is empty. Should check SpeciesLink only. working on it")
+
+  message("Gbif is empty. Working with SpeciesLink only.")
+
+  check_status_spLink <- check_string(scientificName = spLink$scientificName)
+  head(check_status_spLink)
+  spLink$scientificName <- check_status_spLink$scientificName_new
+  spLink$scientificStatus <- check_status_spLink$scientificName_status
+  spLink <- subset(spLink, scientificStatus != "indet")
+
+  #nrow(spLink)
+  #nrow(gbif)
+  #View(gbif)
+
+  #Get onlye species names, lat, long and ID. Rename columns of the two datasets to be the same. Add a column that provides info about data source.
+
+  splink_table <- data.frame(spLink$record_id,spLink$scientificName, spLink$decimalLatitude, spLink$decimalLongitude, spLink$scientificStatus)
+
+  splink_table$DataSource <- "SpeciesLink"
+  names(splink_table)[names(splink_table) == "spLink.record_id"] <- "ID"
+  names(splink_table)[names(splink_table) == "spLink.scientificName"] <- "species"
+  names(splink_table)[names(splink_table) == "spLink.decimalLatitude"] <- "decimalLatitude"
+  names(splink_table)[names(splink_table) == "spLink.decimalLongitude"] <- "decimalLongitude"
+  names(splink_table)[names(splink_table) == "spLink.scientificStatus"] <- "scientificStatus"
+
+    # output w/ only potential correct coordinates
+
+  # Cleaning data -------------------------------------
+
+  #Clean NAs
+  splink_table_coord <- merge[!is.na(splink_table$decimalLatitude) & !is.na(splink_table$decimalLongitude),]
+  #Set rownames to NULL so it does not get errors in geo_clean function
+  rownames(splink_table_coord) <- NULL
+
+  #In case geo_clean finds specific rows with errors, use this to clean it. Then continue manually.
+  #merge_coord <- merge_coord[-c(row1, row2, row1...), ]
+
+  #nrow(merge_coord)
+  #View(merge_coord)
+
+  #Now we will use the the function `clean_coordinates()` from the `CoordinateCleaner` package to clean the species records. This function checks for common errors in coordinates such as institutional coordinates, sea coordinates, outliers, zeros, centroids, etc. This function does not accept not available information (here addressed as "NA") so we will first select only data that have a numerical value for both latitude and longitude.
+
+  #Note: at this moment having a specific ID code for each observation is essential. The raw data already provides an ID in the column `gbifID`.
+
+  geo.clean <- clean_coordinates(x = splink_table_coord,
+                                 lon = "decimalLongitude",
+                                 lat = "decimalLatitude",
+                                 species = "species",
+                                 value = "clean")
+
+  #nrow(geo.clean)
+  #View(geo.clean)
+  duplicata <- duplicated(geo.clean[, c('decimalLongitude', 'decimalLatitude')])
+  #which(duplicata)
+  #sum(duplicata)
+  geo.clean2 <- geo.clean[!duplicata, ]
+  #nrow(geo.clean2)
+
+
+  #plotting in maps to see points of occurence (if you want to keep track of what's going on)
+  #par(mfrow = c(1, 2))
+  #plot(decimalLatitude ~ decimalLongitude, data = merge)
+  #map(, , , add = TRUE)
+  #plot(decimalLatitude ~ decimalLongitude, data = geo.clean2)
+  #map(, , , add = TRUE)
+  #par(mfrow = c(1, 1))
+
+
+  #############################################
+  ########### Important: TO DO
+  ##Check if species has more than 20 occurences in total after cleaning. If not, than it can't be used in ENM and should not be save (Stockwell & Peterson, 2002).
+  #############################################
+
+  ### If is has more than 20 occurences, than keep it and sabe it to a csv file.
+  write.csv(geo.clean2,
+            paste0("./results/clean_data/clean_data_",scientificName[a],".csv"),
+            row.names = FALSE)
+
 
   }
 
@@ -150,36 +230,46 @@ gbif <- read.csv(file_path2)
 ########TAXONOMIC CLEANING
 
 ##Check status - function by Sara Mortara
+
 check_status_gbif <- check_string(scientificName = gbif$scientificName)
-head(check_status_gbif)
+#head(check_status_gbif)
+gbif$scientificStatus <- check_status_gbif$scientificName_status
 gbif$scientificName <- check_status_gbif$scientificName_new
+##WORK ON THIS
+##Here the script needs to replace name only IF NAME IS VALID (e.g. if "indet" > discart )
+gbif <- subset(gbif, scientificStatus != "indet")
 
 check_status_spLink <- check_string(scientificName = spLink$scientificName)
-head(check_status_spLink)
+#head(check_status_spLink)
 spLink$scientificName <- check_status_spLink$scientificName_new
+spLink$scientificStatus <- check_status_spLink$scientificName_status
+spLink <- subset(spLink, scientificStatus != "indet")
 
 #nrow(spLink)
 #nrow(gbif)
 #View(gbif)
+#View(spLink)
 
 #Merge the two tables
 #Get onlye species names, lat, long and ID. Rename columns of the two datasets to be the same. Add a column that provides info about data source.
 
-splink_table <- data.frame(spLink$record_id,spLink$scientificName, spLink$decimalLatitude, spLink$decimalLongitude)
+splink_table <- data.frame(spLink$record_id,spLink$scientificName, spLink$decimalLatitude, spLink$decimalLongitude, spLink$scientificStatus)
 
 splink_table$DataSource <- "SpeciesLink"
 names(splink_table)[names(splink_table) == "spLink.record_id"] <- "ID"
 names(splink_table)[names(splink_table) == "spLink.scientificName"] <- "species"
 names(splink_table)[names(splink_table) == "spLink.decimalLatitude"] <- "decimalLatitude"
 names(splink_table)[names(splink_table) == "spLink.decimalLongitude"] <- "decimalLongitude"
+names(splink_table)[names(splink_table) == "spLink.scientificStatus"] <- "scientificStatus"
 
-gbif_table <- data.frame(gbif$key, gbif$scientificName, gbif$decimalLatitude, gbif$decimalLongitude)
+gbif_table <- data.frame(gbif$key, gbif$scientificName, gbif$decimalLatitude, gbif$decimalLongitude, gbif$scientificStatus)
 
 gbif_table$DataSource <- "GBIF"
 names(gbif_table)[names(gbif_table) == "gbif.key"] <- "ID"
 names(gbif_table)[names(gbif_table) == "gbif.scientificName"] <- "species"
 names(gbif_table)[names(gbif_table) == "gbif.decimalLatitude"] <- "decimalLatitude"
 names(gbif_table)[names(gbif_table) == "gbif.decimalLongitude"] <- "decimalLongitude"
+names(gbif_table)[names(gbif_table) == "gbif.scientificStatus"] <- "scientificStatus"
 
 #merge the two datasets to clean them together
 merge <- bind_rows(splink_table, gbif_table)
@@ -195,6 +285,9 @@ merge <- bind_rows(splink_table, gbif_table)
 merge_coord <- merge[!is.na(merge$decimalLatitude) & !is.na(merge$decimalLongitude),]
 #Set rownames to NULL so it does not get errors in geo_clean function
 rownames(merge_coord) <- NULL
+
+#In case geo_clean finds specific rows with errors, use this to clean it. Then continue manually.
+#merge_coord <- merge_coord[-c(row1, row2, row1...), ]
 
 #nrow(merge_coord)
 #View(merge_coord)
